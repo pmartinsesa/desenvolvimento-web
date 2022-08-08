cb4b39f43d3a45c1330b1cfce11f575530de711f
pmartinsesa <pmartinsesa@gmail.com>
2022-08-08 19:47:05 -0300

    move funciona

--- Trabalho3/src/canvasHandler.js
+++ Trabalho3/src/canvasHandler.js
@@ -1,26 +1,71 @@
+console.log("oi")
+
+const lineBeginX = 250;
+const lineBeginY = 250;
+const lineEndX = 450;
+const lineEndY = 250;
+const lineLength = lineEndX - lineBeginX;
+
+
+const primeiroParametro = 315
+const segundoParametro = 405
+
+
 const canvas = $("#myCanvas");
 const ctx = canvas[0].getContext("2d");
-const line = new Path2D();
+let line = new Path2D();
 
 ctx.lineWidth = 10;
-line.moveTo(250, 250);
-line.lineTo(450, 250);
+line.moveTo(lineBeginX, lineBeginY);
+line.lineTo(lineEndX, lineEndY);
 
 console.log(line)
 ctx.stroke(line);
 
 const moveu = (e) => {
-    console.log("mousemove");
+    const isCenter = e.offsetX > 300 && e.offsetX < 400;
+    const isRight = e.offsetX <= 300;
+    const isLeft = !isRight;
+    let command = 'center'
+
+    if (isCenter) { 
+        console.log("isCenter", isCenter);
+    }
+    else {
+        command = 'growLine';
+        if (isRight) {
+            console.log("isRight", isRight);
+        }
+        else {
+            console.log("isLeft", isLeft);
+        }
+    }
+
+    // if(command === 'growLine') {
+    //     console.log(e.offsetX)
+    //     ctx.clearRect(0, 0, 700, 500);
+    //     console.log(line)
+    //     line.moveTo(lineBeginX, lineBeginY);
+	// 	ctx.lineTo(e.offsetX, e.offsetY);
+    //     ctx.stroke(line);
+    // }
+    // else {
+    //     console.log("center")
+
+        ctx.clearRect(0, 0, 700, 500);
+        line = new Path2D();
+        line.moveTo(e.offsetX - (lineLength /2), e.offsetY);
+        line.lineTo(e.offsetX + (lineLength /2), e.offsetY);
+        ctx.stroke(line);
+    // }
 };
 
 
 canvas.on("mousedown", (e) => {
-    console.log("mousedown");
+    // console.log("mousedown", e.offsetX);
     const hasClickInSomeLine = ctx.isPointInStroke(line, e.offsetX, e.offsetY)
     if (hasClickInSomeLine) {
-        // canvas.on("mousemove", moveu);
         document.getElementsByTagName("canvas")[0].addEventListener("mousemove", moveu, false);
-
     }
 });
 
@@ -29,7 +74,7 @@ canvas.on("mousedown", (e) => {
 // });
 
 canvas.on("mouseup", (e) => {
-    console.log("mouseup")
+    // console.log("mouseup")
     //console.log(canvas.removeEventListener("mousemove", moveu, false))
     document.getElementsByTagName("canvas")[0].removeEventListener("mousemove", moveu, false);
 });
